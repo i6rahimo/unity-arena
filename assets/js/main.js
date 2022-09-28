bdc6ed124ed63d3991549758452fef5c4fb71be9
i6rahim <i6rahim210@gmail.com>
2022-09-28 07:34:09 +0300

    "sections"

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,39 @@ const swiper = new Swiper('.swiper', {
     },
    
   });
+  const roomSwiper = new Swiper('.room-swiper', {
+    // Optional parameters
+    slidesPerView: 3,
+    direction: 'horizontal',
+    spaceBetween: 20,
+    slideClass: 'room-slide',
+    wrapperClass: 'room-wrapper',
+    scrollbar: {
+      el: '.room-swiper-scrollbar',
+      // dragSize: '174',
+      draggable: true,
+    },
+    pagination: {
+      el: '.stock-swiper-pagination',
+      type: 'fraction',
+    },
+    breakpoints: {
+      700: {
+        slidesPerView: 3,
+    spaceBetween: 20,
 
+      },
+      500: {
+        slidesPerView: 2,
+    spaceBetween: 10,
 
+      },
+      360: {
+    spaceBetween: 0,
+    slidesPerView: 1,
+      }
+    }
+  });
 
   const headerNav = document.querySelector('.header__nav'),
         headerBtn = document.querySelector('.header__close'),
@@ -49,3 +80,39 @@ const swiper = new Swiper('.swiper', {
     SpanUp.classList.toggle('open')
     SpanMiddle.classList.toggle('open')
   })
+
+
+  const accordion = document.querySelectorAll('.price__accordion-block');
+  const accordionArrow = document.querySelectorAll('.arrow-img');
+  accordion.forEach((e)=> {
+    e.addEventListener('click', (el) => {
+    e.classList.toggle('show')
+  })})
+  
+const select = document.querySelector('.select-form').addEventListener('click', showSelect);
+const selectList = document.querySelector('.select-list');
+const selectListP = document.querySelectorAll('.select-list p');
+const selectInput = document.querySelector('.select-form-input');
+const selectArrow = document.querySelector('.custom-arrow');
+function showSelect() {
+  selectList.classList.toggle('show');
+  selectArrow.classList.toggle('rotate')
+}
+
+selectListP.forEach(e => {
+  e.addEventListener('click', (el)=> {
+    selectInput.value = e.innerHTML;
+  })
+})
+
+const data = document.querySelector('.data');
+data.textContent = '12 сентября 2022'
+
+const playernum = document.querySelector('#playernumber').addEventListener('input', chekPlayer);
+function chekPlayer(e) {
+  if(e.value > 24 ) {
+    alert( 'Не больше 24 игроков' )
+    console.log(e);
+  }
+  return playernum
+}
